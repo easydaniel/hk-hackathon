150f03b2aead26606b8e0f37a7cc5ae2b2f53939
toosyou <toosyou70207@gmail.com>
2016-03-03 02:02:20 +0800

    get database_name & collection_names done

--- data_analysis/analysis.py
+++ data_analysis/analysis.py
@@ -6,15 +6,21 @@ import sys
 
 if __name__ == '__main__':
 	#connect mongodb
-	#password_raw = getpass.getpass('Input password of db : ')
-	#password_db = urllib.quote_plus(password_raw)
 
 	try :
-		db_client = MongoClient('mongodb://140.113.89.72',serverSelectionTimeoutMS=500)
-		db_client.server_info()
+		client_db = MongoClient('mongodb://140.113.89.72',serverSelectionTimeoutMS=500)
+		client_db.server_info()
 
 	except errors.ServerSelectionTimeoutError as err :
 		print(err)
 		sys.exit(0)
 
-	
+	#find the database is not named 'local'
+	for name in client_db.database_names():
+		if name != 'local':
+			database = client_db[name]
+			break
+
+	#get data from name_database
+	for name in database.collection_names():
+		print(name)
